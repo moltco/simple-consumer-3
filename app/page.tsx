438e39110b11033716e6da60e6df623d3aa82ea5
moltco <67069242+moltco@users.noreply.github.com>
2024-09-10 16:51:42 +0100

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
 /** Add your relevant code here for the issue to reproduce */
+import { ExportedClass, ExportedEnum, ExportedType } from "simple-package";
+
 export default function Home() {
+  const msg: ExportedType = {
+    message: "I am warning you",
+    severity: ExportedEnum.warning,
+  };
+
+  const importedClass = new ExportedClass(msg);
+  console.log(importedClass.toExportString());
+
   return null;
 }
